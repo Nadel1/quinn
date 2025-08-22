19e85ffe1e8a6d50dbecf3330b473850fc794b84
Natalia Piotrowski <n.piotrowski@tum.de>
2025-08-22 16:45:43 +0200

    start working on cr in quinn, heavily based on quiche implementation

--- quinn-proto/src/congestion/cubic.rs
+++ quinn-proto/src/congestion/cubic.rs
@@ -3,6 +3,7 @@ use std::cmp;
 use std::sync::Arc;
 
 use super::{BASE_DATAGRAM_SIZE, Controller, ControllerFactory};
+use crate::resume;
 use crate::connection::RttEstimator;
 use crate::{Duration, Instant};
 
@@ -10,6 +11,7 @@ use crate::{Duration, Instant};
 ///
 /// These are recommended value in RFC8312.
 const BETA_CUBIC: f64 = 0.7;
+const SAVED_CC_FILE: &str = "saved_params_quinn.csv";
 
 const C: f64 = 0.4;
 
