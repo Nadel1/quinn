d8d536c9a09a4534f27d2a4ccd3c61d034234bcb
Natalia Piotrowski <n.piotrowski@tum.de>
2026-02-24 15:01:34 +0100

    did not create logging file

--- quinn-proto/src/connection/mod.rs
+++ quinn-proto/src/connection/mod.rs
@@ -370,6 +370,17 @@ impl Connection {
             this.write_crypto();
             this.init_0rtt();
         }
+        if this.logging_name != "" {
+            File::create(this.logging_name.clone()).unwrap();
+            use std::io::Write; //has to be included here, otherwise issues with other write calls
+            let mut file = File::options()
+                .append(true)
+                .open(this.logging_name.clone())
+                .unwrap();
+            let save_string =
+                "TIMESTAMP,SENT/RECEIVED,PACKET_NUM,PACKET_SIZE,CWND,BYTES_IN_FLIGHT,RTT\n";
+            let _ = file.write_all(save_string.as_bytes());
+        }
         this
     }
 
